eeb92fefe88b784382b2e49de483938f6e40a3d8
your name here <your_email_here>
2019-12-15 02:56:30 -0500

    finished score

--- index.js
+++ index.js
@@ -1,90 +1,82 @@
 // document.addEventListener('DOMContentLoaded', () => {
     let playerHand = {};
+    let player=document.querySelector("#player")
     let houseHand = {};
+    let house=document.querySelector("#house")
     let playerSum =0;
     let houseSum=0;
-    let id=""
-
+    let id;
+    let hit = document.createElement("button");
+    hit.innerText="HIT";
+    let stay = document.createElement("button");
+    stay.innerText="STAY";
+    let twoBut=document.querySelector("#twoBut")
+    
     const newDeckCards = async () => {
         try {
             let newDeck = await axios.get("https://deckofcardsapi.com/api/deck/new/")
-            let deck_id = newDeck.data.deck_id;
-            // let id = document.querySelector("#deckId");
             id = newDeck.data.deck_id;
-            // id.innerHTML=newDeck.data.deck_id;
-            let shuffle = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/shuffle/`)
+            let shuffle = await axios.get(`https://deckofcardsapi.com/api/deck/${id}/shuffle/`)
             let button = document.querySelector('#button');
-            button.parentNode.removeChild(button);
+            button.parentNode.removeChild(button)
+            twoBut.appendChild(hit)
+            twoBut.appendChild(stay)
+            // button.parentNode.replaceChild((hit,stay),button)
             displayCard();
+
         } catch (err) {
             console.log(err)
             debugger
         }
     }
     
-    const drawCard=async(numCard, hand) =>{
-        let id = document.querySelector("#deckId");
-        let drawCard=await axios.get(`https://deckofcardsapi.com/api/deck/${id.value}/draw/?count=${numCard}`);
+    const drawCard=async(numCard,hand,side) =>{
+        // let id = document.querySelector("#deckId");
+        let drawCard=await axios.get(`https://deckofcardsapi.com/api/deck/${id}/draw/?count=${numCard}`);
         drawCard.data.cards.forEach(card=>{
             let ol = document.createElement("ol");
             let image=document.createElement("img");
             image.src = card.image;
             ol.appendChild(image);
-            document.body.appendChild(ol);
+            side.appendChild(ol);
             hand[card.value] ? hand[card.value]+=1:hand[card.value]=1;
-        }
-        )
-        score(playerHand);
+        })
+        score(hand);
     }
 
     const displayCard = async() => {
-        let hit = document.createElement("button");
-        hit.innerText="HIT";
-        let stay = document.createElement("button");
-        stay.innerText="STAY";
-        document.body.appendChild(hit);
-        document.body.appendChild(stay);
 
-        drawCard(2,playerHand);
+        drawCard(3,playerHand,player);
 
         hit.addEventListener("click", ()=>{
-            drawCard(1,playerHand);
-            console.log(playerHand)
+            drawCard(1,playerHand,player);
+            // console.log(playerHand)
         })
         stay.addEventListener("click", ()=>{
-            drawCard(3,houseHand);
-
-            console.log(houseHand)
+            drawCard(3,houseHand,house);
+            // console.log(houseHand)
         })
-        
     }
     
     const score =(hand)=>{
-        // let tenPoint=["KING", "JACK", "QUEEN"]
+        playerSum=0;
         for(let value in hand){
-            if(value==="KING"||value==="JACK"||value==="QUEEN"){
-                playerSum+=10
+            if(value==="2"||value==="3"||value==="4"||value==="5"||value==="6"||value==="7"||value==="8"||value==="9"||value==="10"){
+                playerSum+=Number(hand[value]*value)
+                console.log(value)
+            }else if(value==="KING"||value==="JACK"||value==="QUEEN"){
+                playerSum+=Number(hand[value]*10)
                 console.log("add 10")
             }else if(value==="ACE"){
-                playerSum+=11;
-                console.log("add 11")
+                playerSum+=Number(hand[value]*11);
+                console.log("add11")
                 if(playerSum>21){
-                    playerSum-=11;
-                    console.log("mins 11")
-                }else{
-                    playerSum+=1
-                    console.log("add 1")
+                    playerSum-=Number(hand[value]*11);
+                    playerSum+=Number(hand[value]*1);
                 }
-            }else{
-                playerSum+=Number(value)
-                // console.log(`${playerSum} include ${Number(value)}`)
             }
-        // let score = document.createElement("ul");
-        // console.log(hand);
-        // score.innerText=playerSum
-        // console.log(playerSum)
-        // document.body.appendChild(score);
     }
+    console.log(`total is ${playerSum}`)
 }
     // let h1=document.querySelector("h1")
    // if(total===21){
